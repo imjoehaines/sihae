bd4111112ad759d7757febc2dad484bfc89f6508
Joe Haines <hello@joehaines.co.uk>
2015-09-12 17:06:53 +0100

    Remove bootstrap & setup phpspec

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir')
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +11,6 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
-});
+elixir(function (mix) {
+  mix.phpSpec()
+})
