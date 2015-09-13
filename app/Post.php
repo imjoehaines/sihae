85d2ded202973485d66b52236101d788f2cc2912
Joe Haines <hello@joehaines.co.uk>
2015-09-13 14:09:44 +0100

    Docblocks

--- app/Post.php
+++ app/Post.php
@@ -12,11 +12,21 @@ class Post extends Model
      */
     protected $table = 'posts';
 
+    /**
+     * Gets the date a post was created
+     *
+     * @return Carbon
+     */
     public function dateCreated()
     {
         return Carbon::createFromFormat('Y-m-d H:i:s', $this->created_at);
     }
 
+    /**
+     * Gets the time since the date a post was created - e.g. "5 days ago"
+     *
+     * @return string
+     */
     public function timeSinceDateCreated()
     {
         return $this->dateCreated()->diffForHumans();
