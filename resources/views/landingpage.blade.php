e25a5181582b3774a98e9a7c9f382e92e5dad97a
Joe Haines <hello@joehaines.co.uk>
2015-09-13 12:05:33 +0100

    Add some more placeholder text

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,5 +1,11 @@
 @extends('layout')
 
 @section('content')
-  <p>Testing</p>
+  <p>Ex utroque dissentias mel, an mei probo animal contentiones. Graeco tincidunt cu pri. Sea ex nominati disputando. Postea option hendrerit per eu. Inani urbanitas sententiae no cum, aliquip repudiare vix ad. Ne putant detracto pro. Sit inani clita veritus ne, qui te dicant consetetur, facer consectetuer at eos.</p>
+
+  <p>Sit magna nullam no, eos et melius deserunt. Ut labore aliquam similique eos. No ius facete accusam copiosae. Philosophia theophrastus et cum, cum et errem graeco expetendis. Te vix vivendo tibique elaboraret, noster adolescens an duo, zril dolore ad nec.</p>
+
+  <p>Id novum labore interesset eam. Adipiscing reformidans id mei, bonorum electram ad per. Ius indoctum elaboraret temporibus ea. Id duo laoreet omittantur. Legendos neglegentur mediocritatem usu in, esse vero reformidans at nec, vis meis iudicabit dignissim ad. Rationibus reprimique ad usu, probo illum an vim. Quo sint paulo eu, per quaestio accusamus repudiandae id, mea wisi delicata interpretaris at.</p>
+
+  <p>Nec in animal expetenda dissentias. Cu sed graece nominati, per hendrerit intellegebat ut. Dico platonem ei vix, impedit epicurei splendide vix id. Dicit omnes adversarium ne his, ius ancillae sadipscing persequeris in. Vim ne putent corpora. Has aeterno adolescens an. Vim modus vocibus complectitur ea.</p>
 @endsection
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -5,3 +5,5 @@
   <li>Two</li>
   <li>Three</li>
 </ul>
+
+<p>Alienum deterruisset mea an, at eos albucius adipiscing, mea ex viderer menandri facilisis. An inimicus partiendo qualisque quo, ex qui aperiri constituto. Ne deleniti splendide sed, id mea aperiri imperdiet. Et sed veniam definitionem, sit eu erant integre delectus. Ut sea erat lobortis iracundia. Vim timeam conceptam ut, sed antiopam senserit comprehensam cu, an mel facer cetero.</p>
