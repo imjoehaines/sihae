775bbb2248f8956c17a652b567e2ac3a4992183f
Joe Haines <hello@joehaines.co.uk>
2015-09-13 23:09:25 +0100

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@inject('config', 'Sihae\Providers\ConfigServiceProvider')
+@extends('layout')
+
+@section('content')
+  <h2>Oops!</h2>
+  <p>I couldn't find the page you requested, maybe you could try <a href="/">this one</a> instead.</p>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
 <html>
 <head>
   <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,400italic' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/sihae.css">
+  <link rel="stylesheet" href="/css/normalize.css">
+  <link rel="stylesheet" href="/css/sihae.css">
 
   <title>{{ $config::get('title') }}</title>
 </head>
