2c0a74d89958bb8f59ddd21f6a1edd38b8ce89f2
Joe Haines <hello@joehaines.co.uk>
2015-09-20 13:27:48 +0100

    Test admin user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(PostSeeder::class);
+        $this->call(UserSeeder::class);
 
         Model::reguard();
     }
