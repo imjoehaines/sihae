72024e876fd4f1479157d4e907abe2443c457cbd
Joe Haines <hello@joehaines.co.uk>
2016-10-01 18:42:40 +0100

    Add 404 handler

--- config/middleware.php
+++ config/middleware.php
@@ -1,4 +1,17 @@
 <?php
-// Application middleware
 
-// e.g: $app->add(new \Slim\Csrf\Guard);
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+// 404 Handler
+$app->add(function (Request $request, Response $response, callable $next) use ($container) {
+    $response = $next($request, $response);
+
+    if ($response->getStatusCode() == 404) {
+        $handler = $container['notFoundHandler'];
+
+        return $handler($request, $response);
+    }
+
+    return $response;
+});
