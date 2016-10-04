eb46e33632a31c01075ab2bfdae2371848866436
Joe Haines <hello@joehaines.co.uk>
2016-10-04 22:05:35 +0100

    I made a login form

--- config/routes.php
+++ config/routes.php
@@ -10,4 +10,16 @@ return function (App $app) {
     $app->post('/post/edit/{slug}', 'Sihae\PostController:update');
     $app->get('/post/delete/{slug}', 'Sihae\PostController:delete');
     $app->get('/post/{slug}', 'Sihae\PostController:show');
+
+    $app->get('/login', function ($request, $response) {
+        return $this->get('renderer')->render($response, 'layout.phtml', ['page' => 'login']);
+    });
+
+    $app->post('/login', function ($request, $response) {
+        return $response->withStatus(302)->withHeader('Location', '/');
+    });
+
+    $app->get('/logout', function ($request, $response) {
+        return $response->withStatus(302)->withHeader('Location', '/');
+    });
 };
