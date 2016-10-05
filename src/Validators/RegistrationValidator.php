4ad3f119fb28cc3f5fe145697e9ab99fef41a1f7
Joe Haines <hello@joehaines.co.uk>
2016-10-05 21:35:13 +0100

    Update & use schemer 2.1
    More entirely original code that was not written by someone else
    
    honest

--- src/Validators/RegistrationValidator.php
+++ src/Validators/RegistrationValidator.php
@@ -2,7 +2,6 @@
 
 namespace Sihae\Validators;
 
-use Schemer\Result;
 use Schemer\Validator as V;
 use Schemer\Formatter as F;
 
@@ -18,25 +17,28 @@ class RegistrationValidator implements Validator
             'Username' => V::text()->min(3)->max(50)->alphanum(),
             'Password' => V::text()->min(7),
             'Password confirmation' => V::text()->min(7),
-        ]);
+        ])->should(
+            function ($values) {
+                return $values['Password'] === $values['Password confirmation'];
+            },
+            'Passwords didn\'t match!'
+        );
 
         $this->formatter = F::assoc([
             'username' => F::text(),
             'password' => F::text(),
             'password_confirmation' => F::text(),
-        ])->rename('username', 'Username')
-          ->rename('password', 'Password')
-          ->rename('password_confirmation', 'Password confirmation');
+        ])->renameMany([
+            'username' => 'Username',
+            'password' => 'Password',
+            'password_confirmation' => 'Password confirmation'
+        ]);
     }
 
     public function isValid(array $userDetails) : bool
     {
         $detailsToValidate = $this->formatter->format($userDetails);
         $result = $this->validator->validate($detailsToValidate);
-
-        if ($userDetails['password'] !== $userDetails['password_confirmation']) {
-            $result = Result::failure('Passwords did not match')->concat($result);
-        }
 
         $this->errors = $result->errors();
 
