65c5cd19d2f32e63fd8c72aae8471b96a34e4f6f
Joe Haines <hello@joehaines.co.uk>
2016-10-07 23:47:16 +0100

    Update routes with new namespaces

--- config/routes.php
+++ config/routes.php
@@ -4,24 +4,24 @@ use Slim\App;
 use Sihae\Middleware\AuthMiddleware;
 
 return function (App $app) {
-    $app->get('/[page/{page:[1-9][0-9]*}]', 'Sihae\PostController:index');
+    $app->get('/[page/{page:[1-9][0-9]*}]', 'Sihae\Controllers\PostController:index');
 
     $app->group('/', function () {
-        $this->get('new', 'Sihae\PostController:create');
-        $this->post('new', 'Sihae\PostController:store');
-        $this->get('edit/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\PostController:edit');
-        $this->post('edit/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\PostController:update');
-        $this->get('delete/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\PostController:delete');
+        $this->get('new', 'Sihae\Controllers\PostController:create');
+        $this->post('new', 'Sihae\Controllers\PostController:store');
+        $this->get('edit/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\Controllers\PostController:edit');
+        $this->post('edit/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\Controllers\PostController:update');
+        $this->get('delete/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\Controllers\PostController:delete');
     })->add(AuthMiddleware::class);
 
-    $app->get('/archive', 'Sihae\ArchiveController:index');
+    $app->get('/archive', 'Sihae\Controllers\ArchiveController:index');
 
-    $app->get('/post/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\PostController:show');
+    $app->get('/post/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\Controllers\PostController:show');
 
-    $app->get('/login', 'Sihae\LoginController:showForm');
-    $app->post('/login', 'Sihae\LoginController:login');
-    $app->get('/logout', 'Sihae\LoginController:logout');
+    $app->get('/login', 'Sihae\Controllers\LoginController:showForm');
+    $app->post('/login', 'Sihae\Controllers\LoginController:login');
+    $app->get('/logout', 'Sihae\Controllers\LoginController:logout');
 
-    $app->get('/register', 'Sihae\RegistrationController:showForm');
-    $app->post('/register', 'Sihae\RegistrationController:register');
+    $app->get('/register', 'Sihae\Controllers\RegistrationController:showForm');
+    $app->post('/register', 'Sihae\Controllers\RegistrationController:register');
 };
