9839aaac17ddc8bec5e0221c7b0ffd4433aff159
Joe Haines <hello@joehaines.co.uk>
2016-10-16 21:38:11 +0100

    Start building a proper static pages section

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,7 @@
 
 use Slim\App;
 use Sihae\Middleware\AuthMiddleware;
+use League\CommonMark\CommonMarkConverter;
 
 return function (App $app) {
     $app->get('/[page/{page:[1-9][0-9]*}]', 'Sihae\Controllers\PostController:index');
@@ -15,10 +16,21 @@ return function (App $app) {
     })->add(AuthMiddleware::class);
 
     $app->get('/archive', 'Sihae\Controllers\ArchiveController:index');
-    $app->get('/about', function ($request, $response) {
-        $renderer = $this->get('Sihae\Renderer');
 
-        return $renderer->render($response, 'about');
+    $app->get('/static/{page}', function ($request, $response, $page) {
+        $renderer = $this->get('Sihae\Renderer');
+        $path = __DIR__ . '/../data/static/' . $page . '.md';
+
+        if (!file_exists($path)) {
+            return $response->withStatus(404);
+        }
+
+        $rawContent = file_get_contents($path);
+
+        $markdown = $this->get(CommonMarkConverter::class);
+        $content = $markdown->convertToHtml($rawContent);
+
+        return $renderer->render($response, 'static', ['content' => $content]);
     });
 
     $app->get('/post/{slug:[a-zA-Z\d\s-_\-]+}', 'Sihae\Controllers\PostController:show');
