a8c25b9bd4d58a8d88070286f6e13c602dcd8f15
Joe Haines <hello@joehaines.co.uk>
2016-10-20 23:14:05 +0100

    Use middleware to locate posts
    Reduces lots of duplication in post controller

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Slim\App;
+use Sihae\Middleware\PostLocator;
 use Sihae\Middleware\AuthMiddleware;
 use Sihae\Controllers\PostController;
 use Sihae\Controllers\LoginController;
@@ -14,13 +15,17 @@ return function (App $app) {
     $app->group('/post', function () {
         $this->get('/new', PostController::class . ':create');
         $this->post('/new', PostController::class . ':store');
-        $this->get('/edit/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':edit');
-        $this->post('/edit/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':update');
-        $this->get('/delete/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':delete');
-        $this->get('/convert/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':convert');
+
+        $this->group('', function () {
+            $this->get('/edit/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':edit');
+            $this->post('/edit/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':update');
+            $this->get('/delete/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':delete');
+            $this->get('/convert/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':convert');
+        })->add(PostLocator::class);
     })->add(AuthMiddleware::class);
 
-    $app->get('/post/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':show');
+    $app->get('/post/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':show')
+        ->add(PostLocator::class);
 
     $app->get('/archive', ArchiveController::class . ':index');
 
@@ -28,7 +33,8 @@ return function (App $app) {
     $app->post('/login', LoginController::class . ':login');
     $app->get('/logout', LoginController::class . ':logout');
 
-    $app->get('/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':show');
+    $app->get('/{slug:[a-zA-Z\d\s-_\-]+}', PostController::class . ':show')
+        ->add(PostLocator::class);
 
     // $app->get('/register', RegistrationController::class . ':showForm');
     // $app->post('/register', RegistrationController::class . ':register');
