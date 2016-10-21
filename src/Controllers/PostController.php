21ff2e56ff1acb87cb7833c55b9691967eb4ab7e
Joe Haines <hello@joehaines.co.uk>
2016-10-21 19:40:39 +0100

    Remove pointless cast

--- src/Controllers/PostController.php
+++ src/Controllers/PostController.php
@@ -110,7 +110,7 @@ class PostController
         return $this->renderer->render($response, 'post-list', [
             'posts' => $parsedPosts,
             'current_page' => $page,
-            'total_pages' => (int) ceil($total / $limit) ?: 1,
+            'total_pages' => ceil($total / $limit) ?: 1,
         ]);
     }
 
