c2b330185c72671ff00ed2ec693f26650cffed98
Joe Haines <hello@joehaines.co.uk>
2017-03-04 17:56:02 +0000

    Update migration to create initial DB state

--- migrations/Version20161028221622.php
+++ migrations/Version20161028221622.php
@@ -5,9 +5,6 @@ namespace Sihae\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20161028221622 extends AbstractMigration
 {
     /**
@@ -15,33 +12,46 @@ class Version20161028221622 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $postTable = $schema->createTable('post');
+        $postTable->addColumn('id', 'integer', ['autoincrement' => true]);
+        $postTable->addColumn('user_id', 'integer');
+        $postTable->addColumn('title', 'string', ['length' => 100]);
+        $postTable->addColumn('slug', 'string');
+        $postTable->addColumn('body', 'text');
+        $postTable->addColumn('is_page', 'boolean');
+        $postTable->addColumn('date_created', 'datetime');
+        $postTable->addColumn('date_modified', 'datetime');
+        $postTable->setPrimaryKey(['id']);
+        $postTable->addUniqueIndex(['slug']);
 
-        $this->addSql('CREATE TABLE post_tag (
-          post_id INT NOT NULL,
-          tag_id INT NOT NULL,
-          INDEX IDX_5ACE3AF04B89032C (post_id),
-          INDEX IDX_5ACE3AF0BAD26311 (tag_id),
-          PRIMARY KEY(post_id, tag_id)
-        ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE tag (
-          id INT AUTO_INCREMENT NOT NULL,
-          name VARCHAR(50) NOT NULL,
-          slug VARCHAR(255) NOT NULL,
-          date_created DATETIME NOT NULL,
-          date_modified DATETIME NOT NULL,
-          UNIQUE INDEX UNIQ_389B783989D9B62 (slug),
-          PRIMARY KEY(id)
-        ) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE
-          post_tag
-        ADD
-          CONSTRAINT FK_5ACE3AF04B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
-        $this->addSql('ALTER TABLE
-          post_tag
-        ADD
-          CONSTRAINT FK_5ACE3AF0BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id)');
+        $userTable = $schema->createTable('user');
+        $userTable->addColumn('id', 'integer', ['autoincrement' => true]);
+        $userTable->addColumn('username', 'string', ['length' => 50]);
+        $userTable->addColumn('password', 'string');
+        $userTable->addColumn('token', 'string', ['length' => 256]);
+        $userTable->addColumn('is_admin', 'boolean');
+        $userTable->addColumn('date_created', 'datetime');
+        $userTable->addColumn('date_modified', 'datetime');
+        $userTable->setPrimaryKey(['id']);
+        $userTable->addUniqueIndex(['username']);
+
+        $tagTable = $schema->createTable('tag');
+        $tagTable->addColumn('id', 'integer', ['autoincrement' => true]);
+        $tagTable->addColumn('name', 'string', ['length' => 50]);
+        $tagTable->addColumn('slug', 'string');
+        $tagTable->addColumn('date_created', 'datetime');
+        $tagTable->addColumn('date_modified', 'datetime');
+        $tagTable->setPrimaryKey(['id']);
+        $tagTable->addUniqueIndex(['slug']);
+
+        $postTagTable = $schema->createTable('post_tag');
+        $postTagTable->addColumn('post_id', 'integer');
+        $postTagTable->addColumn('tag_id', 'integer');
+        $postTagTable->setPrimaryKey(['post_id', 'tag_id']);
+
+        $postTable->addForeignKeyConstraint($userTable, ['user_id'], ['id']);
+        $postTagTable->addForeignKeyConstraint($postTable, ['post_id'], ['id']);
+        $postTagTable->addForeignKeyConstraint($tagTable, ['tag_id'], ['id']);
     }
 
     /**
@@ -49,11 +59,9 @@ class Version20161028221622 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE post_tag DROP FOREIGN KEY FK_5ACE3AF0BAD26311');
-        $this->addSql('DROP TABLE post_tag');
-        $this->addSql('DROP TABLE tag');
+        $schema->dropTable('post_tag');
+        $schema->dropTable('tag');
+        $schema->dropTable('post');
+        $schema->dropTable('user');
     }
 }
